175a49beca79693e8cfb596fcc59237bfebf0918
Greyson Mainor <gbmainor98@outlook.com>
2021-05-18 16:10:02 -0400

    added onto my header with search functionality as well as parameters.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {
+    InputGroup,
+    Input,
+    InputGroupAddon,
+    Button,
+    FormGroup,
+    Label
+} from "reactstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 
 function Header() {
+
+    const [maxResults, setMaxResults] = useState(10)
+    const [startIndex, setStartIndex] = useState(1)
+    const [query, setQuery] = useState("")
+
     return (
-      <div className="main-image d-flex justify-content-center align-items-center flex-column">
-          <div className="filter"></div>
-      </div>
+        <div className="main-image d-flex justify-content-center align-items-center flex-column">
+            <div className="filter"></div>
+            <h1 className="display-2 text-center text-white mb-3">
+                Google Book Search
+          </h1>
+            <div className="text-input">
+                <InputGroup size="lg" className="mb-3">
+                    <Input placeholder="Book Search" />
+                    <InputGroupAddon addonType="append">
+                        <Button color="secondary" className="search-button">
+                            <i className="fas fa-search"></i>
+                        </Button>
+                    </InputGroupAddon>
+                </InputGroup>
+                <div className="d-flex justify-content-center">
+                    <FormGroup className="ml-5">
+                        <Label for="maxResults" className="max-result-label">Max Results</Label>
+                        <Input type="number" id="maxResults" placeholder="Max Results" />
+                    </FormGroup>
+                    <FormGroup className="ml-5">
+                        <Label for="startIndex" className="max-result-label">Start Index</Label>
+                        <Input type="number" id="maxResults" placeholder="Start Index" />
+                    </FormGroup>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
