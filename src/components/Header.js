62c72439d2d47f49dd496ecf72f6dc1b69c8227d
Greyson Mainor <gbmainor98@outlook.com>
2021-05-18 15:20:56 -0400

    styled header component.

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Header() {
+    return (
+      <div className="main-image d-flex justify-content-center align-items-center flex-column">
+          <div className="filter"></div>
+      </div>
+    )
+}
+
+export default Header
